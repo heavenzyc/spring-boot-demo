115fb89889ed934d6570e70a229ab124bd081e26
yuchao.zhang <yuchao.zhang@dmall.com>
2018-10-20 12:50:40 +0800

    修改配置

--- src/main/java/com/heaven/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/heaven/springbootdemo/SpringBootDemoApplication.java
@@ -2,12 +2,25 @@ package com.heaven.springbootdemo;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.heaven.springbootdemo.dao")
-public class SpringBootDemoApplication extends SpringBootServletInitializer {
+@ComponentScan
+@EnableAutoConfiguration(
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class, //（如果使用Hibernate时，需要加）
+                DataSourceTransactionManagerAutoConfiguration.class,
+        }
+)
+public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
